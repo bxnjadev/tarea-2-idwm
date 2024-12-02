6dbae58efde6c03b5bd7c0579d4eefd407436b20
bxnjadev <benjamin.hyx@gmail.com>
2024-12-02 00:55:35 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { initFlowbite } from 'flowbite';
+import { BaseComponent } from './base/base.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+     HeaderComponent,
+    BaseComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tarea-2-idwm';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+
 }
